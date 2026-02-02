da6bd5d377e34aa503fbb71c9ad398084c236d8e
offtopiakurwa-cyber <offtopiakurwa@gmail.com>
2026-02-02 12:03:06 +0500

    Update Main.java

--- src/module2/Practice Work/Main.java
+++ src/module2/Practice Work/Main.java
@@ -1,53 +1,77 @@
 import java.util.List;
 import java.util.ArrayList;
 
-class Usermanager{
-     public final List<User> userList = new ArrayList<>();
+class Usermanager {
 
-     public void addUser(User user){
+    public List<User> userList = new ArrayList<>();
+
+    public void addUser(User user){
         userList.add(user);
-     }
-     public void removeUser(User user){
+    }
+
+    public void removeUser(User user){
         userList.remove(user);
-     }
     }
 
-
-class User{
-    public int id;
-    public String role;
-    public String email;
-
-    public User(int id, String role, String email){
-      this.id = id;
-      this.role = role;
-      this.email = email;
-    }
-    public void updateUserID(int id){
-        this.id = id;
-    }
-    public void updateUserEmail(String email){
-        this.email = email;
-    }
-
-
-    public String toString(){
-      return email + " " + role + " " + id;
+    public void updateUser(User user, String name, String email, String role){
+        user.name = name;
+        user.email = email;
+        user.role = role;
     }
 }
 
-public class Main{
+class User {
+
+    public String name;
+    public String email;
+    public String role;
+
+    public User(String name, String email, String role){
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public void updateName(String name){
+        this.name = name;
+    }
+
+    public void updateEmail(String email){
+        this.email = email;
+    }
+
+    public void updateRole(String role){
+        this.role = role;
+    }
+
+    public String toString(){
+        return name + " " + email + " " + role;
+    }
+}
+
+public class Main {
+
     public static void main(String[] args){
-      Usermanager users = new Usermanager();
-      User user1 = new User(1, "Admin", "1234mail.com");
-      users.addUser(user1);
-      user1.updateUserEmail("EmailUpdatedEmail");
-      System.out.println(user1.toString());
-      User user2 = new User(1, "Admin", "123mail.com");
-      users.addUser(user2);
-      System.out.println(users.userList.size());
-      user2.updateUserID(0);
-      users.removeUser(user2);
-      System.out.println(users.userList.size());
+
+        Usermanager users = new Usermanager();
+
+        User user1 = new User("test1", "alex@mail.com", "Admin");
+        User user2 = new User("test12", "adfs@mail.com", "User");
+
+        users.addUser(user1);
+        users.addUser(user2);
+
+        System.out.println(users.userList.size());
+
+        user1.updateEmail("new@mail.com");
+
+        users.updateUser(user2, "testik", "afds2@mail.com", "Admin");
+
+        System.out.println(user1.toString());
+        System.out.println(user2.toString());
+
+        users.removeUser(user2);
+
+        System.out.println(users.userList.size());
     }
 }
